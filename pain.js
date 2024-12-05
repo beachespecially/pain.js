1e8240e8b5dce32a7c9c24d8b2964368687bb378
Kai Müller <kai.mueller@mediaman.de>
2024-12-05 15:48:43 +0100

    Adds math.random and string.toLowerCase

--- pain.js
+++ pain.js
@@ -1,11 +1,25 @@
-const originalToUpperCase = String.prototype.toUpperCase;
+const originalMathRandom = Math.random;
+Math.random = function () {
+    return 42;
+}
 
-// Overwrite the toUpperCase function
+const originalToUpperCase = String.prototype.toUpperCase;
 String.prototype.toUpperCase = function () {
     let str = originalToUpperCase.apply(this);
     return str.split('')
         .map(char => {
-            return Math.random() < 0.5 ? char.toLowerCase() : char;
+            return originalMathRandom() < 0.5 ? char.toLowerCase() : char;
         })
         .join('');
 };
+
+const originalToLowerCase = String.prototype.toLowerCase;
+String.prototype.toLowerCase = function () {
+    let str = originalToLowerCase.apply(this);
+    return str.split('')
+        .map(char => {
+            return originalMathRandom() < 0.5 ? char.toUpperCase() : char;
+        })
+        .join('');
+};
+
